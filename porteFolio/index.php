4bcdc74a0216352301a6f7b50b22cb17bef4e44b
alphaMarDiallo <alpha.diallo@lepoles.com>
2018-10-19 16:57:39 +0200

    portfolio 20181019

---
+++ porteFolio/index.php
@@ -0,0 +1,190 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Alpha Diallo</title>
+    <!-- CSS bootstrap -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO"
+        crossorigin="anonymous">
+    <!-- CSS perso -->
+    <link rel="stylesheet" href="assets/css/style.css">
+    <!-- font awesome -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU"
+        crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="container-fluid">
+        <nav class="navbar bg-light mb-4">
+            <ul class="nav justify-content-center">
+                <li class="nav-item">
+                    <a class="nav-link active" href="#">A propos de moi</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#competences">Mes compétences</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#experiences">Mes expériences</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#formations">mes formations</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#contact">Contact</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#loisirs">En plus</a>
+                </li>
+            </ul>
+        </nav>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 section" id="aboutMe">
+                    <div class="row">
+
+                        <div class="col-md-6 align-items-center"  >
+                            <img src="assets/img/avatar.jpg" alt="avatar">
+                        </div><!-- /.col-md-6 aboutMe -->
+                        <div class="col-md-6">
+                            <div class="card">
+                                <h5 class="card-header">En quelques mots :</h5>
+                                <div class="card-body">
+                                    <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate,
+                                        alias modi. Rerum facere alias recusandae nemo ea ratione at sapiente? Provident, in
+                                        nemo dignissimos perferendis natus accusamus inventore. Veniam, maiores!
+                                    </p>
+                                </div>
+                            </div>
+                        </div><!-- /.col-md-6 aboutMe -->
+                    </div>
+                </div>
+            </div><!-- FIN .row aboutMe -->
+            <div class="row" >
+                <div class="col-lg-12 section mt-4" id="competences">
+                    <h3>Compétences :</h3>
+                    <ul>
+                        <li>
+                            <figure>
+                                <img src="assets/img/logo/Html5.png" alt="logo HTML 5">
+                                <!-- <figcaption>HTML 5</figcaption> -->
+                            </figure>
+                        </li>
+                        <li>
+                            <figure>
+                                <img src="assets/img/logo//css3.png" alt="logo CSS 3">
+                                <!-- <figcaption>CSS 3</figcaption> -->
+                            </figure>
+                        </li>
+                        <li>
+                            <figure>
+                                <img src="assets/img/logo/javascript.png" alt="logo javascript">
+                                <!-- <figcaption>javascript</figcaption> -->
+                            </figure>
+                        </li>
+                        <li>
+                            <figure>
+                                <img src="assets/img/logo/Bootstrap.png" alt="logo bootstrap">
+                                <!-- <figcaption>Bootstrap</figcaption> -->
+                            </figure>
+                        </li>
+                        <li>
+                            <figure>
+                                <img src="assets/img/logo/jquery.png" alt="logo jQuery">
+                                <!-- <figcaption>jQuery</figcaption> -->
+                            </figure>
+                        </li>
+                        <li>
+                            <figure>
+                                <img src="assets/img/logo/php.png" alt="logo php">
+                                <!-- <figcaption>PHP</figcaption> -->
+                            </figure>
+                        </li>
+                    </ul>
+                </div><!-- FIN .col-lg-12 logo -->
+                <div class="col-lg-12">
+            </div><!-- FIN .row competences -->
+            <div class="row">
+                <div class="col-lg-12">
+                    <ul>
+                        <li>
+                            <figure>
+                                <img src="assets/img/realisation/images_1.jpg" alt="">
+                            </figure>
+                        </li>
+                        <li>
+                            <figure>
+                                <img src="assets/img/realisation/images_2.jpg" alt="">
+                            </figure>
+                        </li>
+                        <li>
+                            <figure>
+                                <img src="assets/img/realisation/images_3.jpg" alt="">
+                            </figure>
+                        </li>
+                        <li>
+                            <figure>
+                                <img src="assets/img/realisation/images_4.jpg" alt="">
+                            </figure>
+                        </li>
+                        <li>
+                            <figure>
+                                <img src="assets/img/realisation/images_5.jpg" alt="">
+                            </figure>
+                        </li>
+                        <li>
+                            <figure>
+                                <img src="assets/img/realisation/images_6.jpg" alt="">
+                            </figure>
+                        </li>
+                    </ul>
+                </div>
+                <!-- /.col-lg-12 -->
+            </div>
+            <!-- /.row -->
+                
+            <div class="row">
+                <div class="col-lg-12 section mt-4" id="experiences">
+
+                </div>
+            </div><!-- FIN .row experiences -->
+            <div class="row">
+                <div class="col-lg-12 section mt-4" id="formations">
+
+                </div>
+            </div><!-- FIN .row formatrions-->
+            <div class="row">
+                <div class="col-lg-12 section mt-4" id="loisirs">
+
+                </div>
+            </div><!-- FIN .row loisirs -->
+            <div class="row">
+                <div class="col-lg-12 section mt-4" id="contact">
+
+                </div>
+            </div><!-- FIN .row contact -->
+
+        </div><!-- FIN .container -->
+        <footer class="footer bg-light mt-4">
+            <div class="container">
+                <span class="text-muted">Place sticky footer content here.</span>
+            </div>
+        </footer>
+    </div><!-- FIN .container-fluid -->
+
+
+
+    <!-- JS bootstrap -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous"> </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
+        crossorigin="anonymous"> </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
+        crossorigin="anonymous"></script>
+    <!-- JS perso -->
+    <script src="assets/js/main.js"></script>
+</body>
+
+</html>
